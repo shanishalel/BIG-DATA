b58418babbdf13480e951465f3cbf21d85b664b6
Shani Shalel <shanishalel89@gmail.com>
2021-07-09 16:23:12 +0300

    first commit

---
+++ Project/kafkaConsume.js
@@ -0,0 +1,51 @@
+// https://www.cloudkarafka.com/ הפעלת קפקא במסגרת ספק זה
+
+const uuid = require("uuid");
+const Kafka = require("node-rdkafka");
+
+const kafkaConf = {
+  "group.id": "cloudkarafka-example",
+  "metadata.broker.list": "dory-01.srvs.cloudkafka.com:9094,dory-02.srvs.cloudkafka.com:9094,dory-03.srvs.cloudkafka.com:9094".split(","),
+  "socket.keepalive.enable": true,
+  "security.protocol": "SASL_SSL",
+  "sasl.mechanisms": "SCRAM-SHA-256",
+  "sasl.username": "urqvma33",
+  "sasl.password": "Vn6M6VlC2XS-oK7zL3QpdEHqse_S6KmU",
+  "debug": "generic,broker,security"
+};
+
+const prefix = "urqvma33-";
+const topic = `${prefix}myTest`;
+const producer = new Kafka.Producer(kafkaConf);
+
+const genMessage = m => new Buffer.alloc(m.length,m);
+//const prefix = process.env.CLOUDKARAFKA_USERNAME;
+
+const topics = [topic];
+const consumer = new Kafka.KafkaConsumer(kafkaConf, {
+  "auto.offset.reset": "beginning"
+});
+
+consumer.on("error", function(err) {
+  console.error(err);
+});
+consumer.on("ready", function(arg) {
+  console.log(`Consumer ${arg.name} ready`);
+  consumer.subscribe(topics);
+  consumer.consume();
+});
+
+consumer.on("data", function(m) {
+ console.log(m.value.toString());
+});
+consumer.on("disconnected", function(arg) {
+  process.exit();
+});
+consumer.on('event.error', function(err) {
+  console.error(err);
+  process.exit(1);
+});
+consumer.on('event.log', function(log) {
+  console.log(log);
+});
+consumer.connect();
---
+++ Project/kafkaProduce.js
@@ -0,0 +1,38 @@
+// https://www.cloudkarafka.com/ הפעלת קפקא במסגרת ספק זה
+
+const uuid = require("uuid");
+const Kafka = require("node-rdkafka");
+const simulator = require('./simulator');
+
+
+const kafkaConf = { 
+  "group.id": "cloudkarafka-example",
+  "metadata.broker.list": "dory-01.srvs.cloudkafka.com:9094,dory-02.srvs.cloudkafka.com:9094,dory-03.srvs.cloudkafka.com:9094".split(","),
+  "socket.keepalive.enable": true,
+  "security.protocol": "SASL_SSL",
+  "sasl.mechanisms": "SCRAM-SHA-256",
+  "sasl.username": "urqvma33",
+  "sasl.password": "Vn6M6VlC2XS-oK7zL3QpdEHqse_S6KmU",
+  "debug": "generic,broker,security"
+};
+
+const prefix = "urqvma33-";
+const topic = `${prefix}myTest`; // send to this topic
+const producer = new Kafka.Producer(kafkaConf);
+
+const genMessage = m => new Buffer.alloc(m.length,m);
+
+producer.on("ready", function(arg) {
+  console.log(`producer Ariel is ready.`);
+  simulator.DataMaker(kafkaConf);
+
+});
+producer.connect();
+
+module.exports.publish= function(msg)
+{   
+  m=JSON.stringify(msg);
+  producer.produce(topic, -1, genMessage(m), uuid.v4());  //Send to KAFKA
+  //producer.disconnect();   
+}
+
