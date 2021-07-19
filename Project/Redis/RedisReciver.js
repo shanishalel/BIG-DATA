0f58bfd9ef18d047c8543a86e52fa784ee71d664
Shani Shalel <shanishalel89@gmail.com>
2021-07-19 17:24:33 +0300

    dashboard is working

--- Project/Redis/RedisReciver.js
+++ Project/Redis/RedisReciver.js
@@ -7,6 +7,11 @@ var sub = redis.createClient()
 
 redisClient.subscribe('message'); 
 
+let all_the_cars=new Map();
+
+let section_1=0,section_2=0,section_3=0,section_4=0,section_5=0;
+let number_of_cars=0;
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
@@ -24,9 +29,86 @@ app.use(function(err, req, res, next) {
 });
 
 redisClient.on("message", function (channel, data) {
-    var data = JSON.parse(data);
-    console.log(data);
+    let car = JSON.parse(data);
+    
+    var cars=new Map();
+    cars.set("Type",car.Type);
+    cars.set("Section",car.Section);
+    cars.set("CarType",car.carType);
+    cars.set("Day", car.Day);
+    cars.set("Time",car.Time);
+    cars.set("IsSpecial",car.IsSpecial);
+
+    all_the_cars.set(number_of_cars,cars);
+    
+    number_of_cars++;
+
+
 });
+
+
+exports.get_sections =(req,res,next) => {
+    all_the_cars.forEach(car => {
+       
+    if(car.Type=="Enter Road" || car.Type=="Enter Section"){
+        switch(car.Section){
+            case 1:
+                section_1++;
+                break;
+            case 2:
+                section_2++;
+                break;
+            case 3:
+                section_3++;
+                break;
+            case 4:
+                section_4++;
+                break;
+            case 5:
+                section_5++;
+                break;
+            default:
+        }
+        
+    
+    }
+    /* at this point we can gets number<0 we should check it ans maybe 
+    change the simulator */
+    if(car.Type=="End Road" || car.Type=="Exit road"){
+        switch(car.Section){
+            case 1:
+                section_1--;
+                break;
+            case 2:
+                section_2--;
+                break;
+            case 3:
+                section_3--;
+                break;
+            case 4:
+                section_4--;
+                break;
+            case 5:
+                section_5--;
+                break;
+            default:
+        }
+
+    }});
+
+    var number_cars=section_1+section_2+section_3+section_4+section_5;
+    var cards=[
+        {section:"1",Number:section_1},
+        {section:"2",Number:section_2},
+        {section:"3",Number:section_3},
+        {section:"4",Number:section_4},
+        {section:"5",Number:section_5},
+        {section:"6",Number:number_cars}];
+
+    res.render('../RT_GUI/views/pages/index',{cards:cards});
+
+};
+
 
 redisClient.on('connect', function() {
     console.log('Reciver connected to Redis');
@@ -35,3 +117,5 @@ redisClient.on('connect', function() {
 server.listen(6061, function() {
     console.log('reciver is running on port 6061');
 });
+
+
--- Project/Redis/RedisSender.js
+++ Project/Redis/RedisSender.js
@@ -6,69 +6,12 @@ var redisClient = redis.createClient();
 var sub = redis.createClient()
 
 // for explanations : https://www.sitepoint.com/using-redis-node-js/
-let section_1=0,section_2=0,section_3=0,section_4=0,section_5=0;
 
 var Db2 = {
    
     InsertCar: function (m) {
-       
-        mJson = JSON.parse(m);
-        if(mJson.Type=="Enter Road" || mJson.Type=="Enter Section"){
-            switch(mJson.Section){
-                case 1:
-                    section_1++;
-                    break;
-                case 2:
-                    section_2++;
-                    break;
-                case 3:
-                    section_3++;
-                    break;
-                case 4:
-                    section_4++;
-                    break;
-                case 5:
-                    section_5++;
-                    break;
-                default:
-            }
-            
-        
-        }
-        /* at this point we can gets number<0 we should check it ans maybe 
-        change the simulator */
-        if(mJson.Type=="End Road" || mJson.Type=="Exit road"){
-            switch(mJson.Section){
-                case 1:
-                    section_1--;
-                    break;
-                case 2:
-                    section_2--;
-                    break;
-                case 3:
-                    section_3--;
-                    break;
-                case 4:
-                    section_4--;
-                    break;
-                case 5:
-                    section_5--;
-                    break;
-                default:
-            }
-   
-        }
-        redisClient.hmset('Sections', "section_1",section_1+"","section_2",section_2+"","section_3",section_3+"", "section_4",section_4+""
-        ,"section_5", section_5+"",function (err, reply) {
-                    console.log(reply);
-        });
-        redisClient.sadd('Cars',m,  function (err, reply) {
-            console.log(reply);
-        });
-        //send the sections to reciver
-        let Sections=[section_1,section_2,section_3,section_4,section_5];
-        redisClient.publish("message",JSON.stringify(Sections),function(){
-            // console.log('published')
+        var car = JSON.parse(m);
+        redisClient.publish("message",JSON.stringify(car),function(){
         });
 
     }
