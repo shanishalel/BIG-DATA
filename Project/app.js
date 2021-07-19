caffbb45f6327795f17b948f701293f0fe52e9bf
Shani Shalel <shanishalel89@gmail.com>
2021-07-19 14:35:33 +0300

    fixing the folders as needed

--- Project/app.js
+++ Project/app.js
@@ -5,8 +5,8 @@ const io = require("socket.io")(server)
 const port = 3000
 
 //------------ kafka------------
-const kafka = require('./kafkaProduce');
-const kafka_consume = require('./kafkaConsume');
+const kafka_produce = require('./Kafka/kafkaProduce');
+const kafka_consume = require('./Kafka/kafkaConsume');
 
 const bodyParser = require('body-parser');
 
@@ -19,14 +19,6 @@ app.use(express.static("public"));
 
 app.get('/', (req, res) => res.send("<a href='/send'>Send</a> <br/><a href=''>View</a>"));
 app.get('/send', (req, res) => res.render('sender'));
-
-// //------------ Socket.io ----------------
-// io.on("connection", (socket) => {
-//     console.log("new user connected");
-//     socket.on("totalWaitingCalls", (msg) => { console.log(msg.totalWaiting) });
-//     socket.on("callDetails", (msg) => { console.log(msg);kafka.publish(msg) });
-
-// });
 
 server.listen(port, () => console.log(`Ariel app listening at http://localhost:${port}`));
 
